a7947bf87ef564596d130aa469be98d36efcc36c
Raphael Brito <raphaellbrito@gmail.com>
2020-05-04 14:11:55 -0300

    routes config

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-import GlobalStyle from './styles/global'
+import Routes from './routes';
+
+import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
   return (
     <>
       <GlobalStyle />
-      <h1>Hello World!</h1>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
     </>
   );
-}
+};
 
 export default App;
